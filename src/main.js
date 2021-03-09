a0e7843305ba27b161efa1ce00ce800abe9e8a73
SamRFreitas <samuelrfreitassi@gmail.com>
2021-03-08 21:49:04 -0300

    Add comentário para atualizar nome do branch

--- src/main.js
+++ src/main.js
@@ -5,6 +5,6 @@ import router from './router'
 import '@/assets/css/tailwind.css'
 import '@/assets/css/fonts.css'
 import 'animate.css'
-
+// Comentário
 const app = createApp(App)
 app.use(router).mount('#app')
