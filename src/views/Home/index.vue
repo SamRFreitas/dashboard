60b741ce7af357d482f024a1ca13ecf33965673c
SamRFreitas <samuelrfreitassi@gmail.com>
2021-03-09 22:35:37 -0300

    Adicionado métodos que escutam a emição de eventos de ativação dos modais de login e criação de conta

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -14,19 +14,29 @@ import CustomHeader from './CustomHeader'
 import { useRouter } from 'vue-router'
 import Contact from './Contact'
 import { onMounted } from 'vue'
+import useModal from '../../hooks/useModal'
 export default {
   components: { CustomHeader, Contact },
 
   setup () {
     const router = useRouter()
+    const modal = useModal()
     onMounted(() => {
       const token = window.localStorage.getItem('token')
       if (token) {
         router.push({ name: 'Feedback' })
       }
     })
-    function handleLogin () {}
-    function handleAccountCreate () {}
+    function handleLogin () {
+      modal.open({
+        component: 'ModalLogin'
+      })
+    }
+    function handleAccountCreate () {
+      modal.open({
+        component: 'ModalCreateAccount'
+      })
+    }
     return {
       handleLogin,
       handleAccountCreate
