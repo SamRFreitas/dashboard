4b9b1f23ae07cc5fb073b37fab371842e9f270f1
SamRFreitas <samuelrfreitassi@gmail.com>
2021-05-03 20:39:44 -0300

    Id's pra teste adicionados

--- src/views/Home/CustomHeader.vue
+++ src/views/Home/CustomHeader.vue
@@ -8,12 +8,14 @@
 
         <div class="flex">
           <button
+            id="header-create-account-button"
             class="px-6 py-2 font-bold rounded-full text-white focus:outline-none"
             @click="() => emit('create-account')"
           >
             Crie uma conta
           </button>
           <button
+            id="header-login-button"
             class="px-6 py-2 font-bold bg-white rounded-full text-brand-main focus:outline-none"
             @click="() => emit('login')"
           >
@@ -34,7 +36,9 @@
         </p>
         <div>
           <button
+            id="cta-create-account-button"
             class="px-6 py-2 mt-10 font-bold bg-white rounded-full text-brand-main focus:outline-none"
+            @click="() => emit('create-account')"
           >
             Crie uma conta grátis
           </button>
