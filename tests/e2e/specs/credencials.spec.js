c8d4fad141e4ea60588aba2197099eec243d63b7
SamRFreitas <samuelrfreitassi@gmail.com>
2021-05-03 21:06:25 -0300

    Teste E2E de credencials para gerdaor de apiKey implementado

---
+++ tests/e2e/specs/credencials.spec.js
@@ -0,0 +1,25 @@
+const APP_URL = process.env.APP_URL || 'http://localhost:8080'
+
+describe('Credencials', () => {
+  it('should login with an email and password', () => {
+    cy.visit(APP_URL)
+
+    cy.get('#header-login-button').click()
+    cy.get('#modal-login')
+
+    cy.get('#email-field').type('igor@igor.me')
+    cy.get('#password-field').type('1234')
+    cy.get('#submit-button').click()
+
+    cy.wait(4000)
+    cy.visit(`${APP_URL}/credencials`)
+    cy.wait(2000)
+
+    const oldApiKey = cy.get('#apiKey').invoke('text')
+    cy.get('#generate-apiKey').click()
+    cy.wait(2000)
+    const newApiKey = cy.get('#apiKey').invoke('text')
+
+    expect(oldApiKey).to.not.equal(newApiKey)
+  })
+})
